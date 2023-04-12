f6d168e95c9b9c66d0fc2058c823bb36f45c5e3e
Soumaya cherichi <soumayacherichi@gmail.com>
2023-04-12 10:18:31 +0100

    boxing

---
+++ practice/BoxingUnboxing/Program.cs
@@ -0,0 +1,23 @@
+﻿// Create an empty list of type object
+List<object> objList = new List<object>();
+
+// Add values to the list
+objList.Add(7);
+objList.Add(28);
+objList.Add(-1);
+objList.Add(true);
+objList.Add("chair");
+
+// Loop through the list and print all values
+foreach (object obj in objList) {
+    Console.WriteLine(obj);
+}
+
+// Add all values that are Int type together and output the sum
+int sum = 0;
+foreach (object obj in objList) {
+    if (obj is int) {
+        sum += (int)obj;
+    }
+}
+Console.WriteLine("The sum of all integer values in the list is: " + sum);
